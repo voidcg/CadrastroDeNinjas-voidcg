191d874d2642b5b80c71b5410e025448a5356ea3
Cauã Cavalcante Gonçalves <gonssalvescaua@gmail.com>
2025-03-14 11:15:22 -0300

    first commit

---
+++ src/main/java/dev/java10x/CadrastroDeNinjas/CadrastroDeNinjasApplication.java
@@ -0,0 +1,13 @@
+package dev.java10x.CadrastroDeNinjas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadrastroDeNinjasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadrastroDeNinjasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/java10x/CadrastroDeNinjas/CadrastroDeNinjasApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java10x.CadrastroDeNinjas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadrastroDeNinjasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
